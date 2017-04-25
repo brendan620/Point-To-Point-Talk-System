876ba618f9a8f60fef3e07633ba54d9cf30996d2
Brendan Lilly <brendan620@gmail.com>
2017-04-25 18:58:37 -0400

    Set up structs in the server

--- client.cpp
+++ client.cpp
@@ -7,3 +7,31 @@
 /*  Start Date:  4/25/2017                                */
 /*  Filename:    client.cpp			                      */
 /**********************************************************/
+
+/**
+                CLIENT
+                @file
+    @author Brendan Lilly
+    @date March 2017
+*/
+
+#include <iostream>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
+#include <string.h>
+using namespace std;
+
+//Constant to set the buffer size as needed
+#define BUFFER_SIZE 500
+
+//Prototypes
+
+
+/**Main method that starts the application
+         @return 0 in success
+                */
+int main(int argc, char* argv[]){
+
+}
--- server.cpp
+++ server.cpp
@@ -7,3 +7,50 @@
 /*  Start Date:  4/25/2017                                */
 /*  Filename:    server.cpp			                      */
 /**********************************************************/
+
+/**
+                LOOKUP_SERVER
+                @file
+    @author Brendan Lilly
+    @date March 2017
+*/
+
+#include <iostream>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+using namespace std;
+
+//Constant to set the buffer size as needed
+#define BUFFER_SIZE 500
+#define MAX_USERS 10
+
+//Structs
+struct tagClient
+{
+	char name[20];
+	struct sockaddr_in serverAddr;
+	struct timeval startTime;
+	struct timeval lastLookupTime;
+	
+};
+
+struct tagDir
+{
+	tagClient clientInfo[MAX_USERS];
+	int numClients;
+};
+//Prototypes
+
+
+/**Main method that starts the application
+         @return 0 in success
+                */
+int main(int argc, char* argv[]){
+
+}
+
