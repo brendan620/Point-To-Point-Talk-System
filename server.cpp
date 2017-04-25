47eeec876988c244f4f03c960d841b6172b2348b
Brendan Lilly <brendan620@gmail.com>
2017-04-25 19:58:10 -0400

    Set up basic server layout

--- server.cpp
+++ server.cpp
@@ -21,6 +21,8 @@
 #include <stdio.h>
 #include <sstream>
 #include <string.h>
+#include <sys/types.h>
+#include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 using namespace std;
@@ -28,6 +30,8 @@ using namespace std;
 //Constant to set the buffer size as needed
 #define BUFFER_SIZE 500
 #define MAX_USERS 10
+#define SERV_HOST_ADDR "156.12.127.18"
+//15080-15089
 
 //Structs
 struct tagClient
@@ -51,6 +55,53 @@ struct tagDir
          @return 0 in success
                 */
 int main(int argc, char* argv[]){
+
+	int sockfd,cliSockFd;
+	socklen_t cliLen;
+	struct sockaddr_in server,client;
+
+	if((sockfd=socket(AF_INET,SOCK_STREAM,0))<0){
+		perror("SERVER CANNOT OPEN SOCKET");
+		exit(0);
+	}
+
+	cout << "SERVER STARTED" << endl;
+
+	//bzero(void *s, size_t n)
+	//erases the data in the n bytes of memory starting
+	//at the location pointed to by s, by writing zeros
+	//to that area
+	bzero((char *) &server,sizeof(server));
+
+	server.sin_family = AF_INET;
+	//htonl == Converts the unsigned integer hostlong from host
+	//byte order to network byte order
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	//htons == Converts the unsigned short integer hostshort
+	//from host byte order to network byte order
+	server.sin_port = htons(15080);
+
+	if((bind(sockfd,(struct sockaddr *) &server,sizeof(server)))<0)
+	{
+		perror("BIND CALL FAILED");
+		exit(0);
+	}
+	cout << "BIND CALL WORKED" << endl;
+
+	listen(sockfd,MAX_USERS);
+	cliLen = sizeof(client);
+
+	while(true){
+
+		if((cliSockFd=accept(sockfd, (struct sockaddr *) &client, &cliLen))<0)
+		{
+			perror("ACCEPT ERROR IN SERVER");
+		}
+		else
+		{
+			cout << "Accepted connection from: " << cliSockFd << endl;
+		}
+	}
 
 }
 
