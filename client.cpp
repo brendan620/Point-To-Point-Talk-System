ccb44696b92f5de173c08b726c985c65582f022f
Brendan Lilly <brendan620@gmail.com>
2017-04-25 18:29:30 -0400

    Added blank files for client and server

---
+++ client.cpp
@@ -0,0 +1,9 @@
+/**********************************************************/
+/*  Edited By:   Brendan Lilly                            */
+/*  Course:      CSC552                                   */
+/*  Professor:   Spiegel                                  */
+/*  Assignment:  3                                        */
+/*  Due Date:    5/11/2017                                */
+/*  Start Date:  4/25/2017                                */
+/*  Filename:    client.cpp			                      */
+/**********************************************************/
